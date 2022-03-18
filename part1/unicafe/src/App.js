a558b7342d85f55047e0ab133225b665b0701bb2
Sam <snippetsbysam@outlook.com>
2022-03-18 00:59:47 +0000

    Exercise 1.10 and 1.11 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,6 +2,15 @@ import { useState } from 'react'
 
 const Button = ( {onClick, text} ) => <button onClick={onClick}>{text}</button>
 
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 const Statistics = ( {feedback} ) => {
   const total = feedback.good + feedback.neutral + feedback.bad
   const average = (feedback.good - feedback.bad) / (feedback.good + feedback.neutral + feedback.bad)
@@ -11,12 +20,19 @@ const Statistics = ( {feedback} ) => {
     return (
       <>
         <h1>statistics</h1>
-        <p>good {feedback.good}</p>
-        <p>neutral {feedback.neutral}</p>
-        <p>bad {feedback.bad}</p>
-        <p>all {total}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
+        <div>
+          <table>
+            <tbody>
+              <StatisticLine text="good" value={feedback.good} />
+              <StatisticLine text="neutral" value={feedback.neutral} />
+              <StatisticLine text="bad" value={feedback.bad} />
+              <StatisticLine text="all" value={total} />
+              <StatisticLine text="average" value={average} />
+              <StatisticLine text="positive" value={positive + " %"} />
+            </tbody>
+          </table>
+        </div>
+        
       </>
     )
   }
